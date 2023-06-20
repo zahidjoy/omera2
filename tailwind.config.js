878671db6aecd98771bebef6fc29a341ee76877e
zahidjoy <imafreelance3@gmail.com>
2023-06-20 20:46:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: "'Poppins', sans-serif",
+			},
+			maxWidth: {
+				container: "1166px",
+			},
+			colors: {
+				primary: "#203239",
+				secondary: "#2032398C",
+				hithlighter: "#F6762E",
+				bannerbg: "#EEEDDE",
+			},
+			zIndex: {
+				100: "100",
+			},
+			boxShadow: {
+				myshadow: "0px 71px 45px -3px rgba(0,0,0,0.22)",
+			},
+		},
+	},
+};
